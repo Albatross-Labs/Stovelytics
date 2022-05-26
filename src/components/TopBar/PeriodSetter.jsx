c42e02302ac1f44164c1f1c0957468a850b86f3a
MinHK4 <dzf4444@gmail.com>
2022-05-26 13:53:29 +0900

    temp PeriodSetter Done

--- src/components/TopBar/PeriodSetter.jsx
+++ src/components/TopBar/PeriodSetter.jsx
@@ -7,11 +7,39 @@ const Wrapper = styled.div`
   height: 100%;
   top: 0;
   right: 0;
-  background-color: pink;
+  // background-color: pink;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const StartDateSet = styled.input`
+  position: absolute;
+  top: 20%;
+  height: 20%;
+  width: 40%;
+  display: flex;
+  align-items: center;
+  font-size: 0.4em;
+`
+
+const EndDateSet = styled.input`
+  position: absolute;
+  top: 60%;
+  height: 20%;
+  width: 40%;
+  display: flex;
+  align-items: center;
+  font-size: 0.4em;
 `
 
 export default function PeriodSetter() {
+
+  
   return (
-    <Wrapper>PeriodSetter</Wrapper>
+    <Wrapper>
+      <StartDateSet placeholder="Start Date ex) 20220515"></StartDateSet>
+      <EndDateSet placeholder="End Date ex) 20220515"></EndDateSet>
+    </Wrapper>
   )
 }
