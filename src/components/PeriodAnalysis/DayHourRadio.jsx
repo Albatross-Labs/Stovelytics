4a05efe54d03e0b2929ff782d8657afec21b5e1f
MinHK4 <dzf4444@gmail.com>
2022-06-01 22:35:20 +0900

    Radio Button added

---
+++ src/components/PeriodAnalysis/DayHourRadio.jsx
@@ -0,0 +1,30 @@
+import React , { useState } from 'react'
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  position: absolute;
+  z-index: 4;
+  width: 10%;
+`
+
+export default function DayHourRadio({selectedValue, ...rest}) {
+
+  return (
+    <Wrapper>
+      <input
+          id="Day"
+          value="Day"
+          type="radio"
+          checked={selectedValue === "Day"}
+          {...rest}
+        />Daily
+        <input
+          id="Hour"
+          value="Hour"
+          type="radio"
+          checked={selectedValue === "Hour"}
+          {...rest}
+        />Hourly
+    </Wrapper>
+  )
+}
--- src/components/PeriodAnalysis/SentimentGraph.jsx
+++ src/components/PeriodAnalysis/SentimentGraph.jsx
@@ -1,9 +1,10 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useState } from 'react'
 import styled from 'styled-components';
 import { Line } from 'react-chartjs-2'
 import { Chart as ChartJS } from 'chart.js/auto';
 
 import { PeriodContext } from '../../contexts/PeriodContext';
+import DayHourRadio from './DayHourRadio';
 
 const Wrapper = styled.div`
   position: relative;
@@ -14,93 +15,114 @@ const Wrapper = styled.div`
   // box-sizing: border-box;
   display: flex;
   justify-content: center;
-  align-items: center;
+  // align-items: center;
 `
 const ChartWrapper = styled.div`
   position: relative;
   width: 98%;
-  height: 95%;
+  height: 90%;
+  bottom: -8%;
+
   background-color: pink;
   // margin: 14px;
   box-sizing: border-box;
+  // overflow: scroll;
 `
 
 export default function SentimentGraph() {
 
   const [period, setPeriod] = useContext(PeriodContext)
-
-
+  const [labelFormat, setLabelFormat] = useState('Day')
 
   // 일별 정보 모으기
-  const dayCount = {}
-  period.map(comment => {
+  if(labelFormat == 'Day'){
+
+    const dayCount = {}
+    period.map(comment => {
     const day = comment.created.substring(5, 10)
     if(!(day in dayCount))
-      dayCount[day] = new Array();
-    dayCount[day].push(comment)
-  })
-
-  const dayLabels = []
-  const dayDatas = []
-  const dayDatasPos = []
-  const dayDatasNeu = []
-  const dayDatasNeg = []
-
-  for(const daily in dayCount){
-    dayLabels.push(daily)
+        dayCount[day] = new Array();
+      dayCount[day].push(comment)
+    })
     
-    var posCnt = 0;
-    var neuCnt = 0;
-    var negCnt = 0;
-    for(const item of dayCount[daily]){
-      if (item.sentiment == '1')
-        posCnt++
-      if (item.sentiment == '0')
-        neuCnt++
-      if (item.sentiment == '-1')
+    const dayLabels = []
+    const dayDatas = []
+    const dayDatasPos = []
+    const dayDatasNeu = []
+    const dayDatasNeg = []
+    
+    for(const daily in dayCount){
+      dayLabels.push(daily)
+      
+      var posCnt = 0;
+      var neuCnt = 0;
+      var negCnt = 0;
+      for(const item of dayCount[daily]){
+        if (item.sentiment == '1')
+          posCnt++
+        if (item.sentiment == '0')
+          neuCnt++
+        if (item.sentiment == '-1')
         negCnt++
-    }
-
-    dayDatas.push(dayCount[daily].length)
-    dayDatasPos.push(posCnt)
-    dayDatasNeu.push(neuCnt)
-    dayDatasNeg.push(negCnt)
-
-  }
-  dayLabels.reverse()
-  dayDatas.reverse()
-
-  dayDatasPos.reverse()
-  dayDatasNeu.reverse()
-  dayDatasNeg.reverse()
-
-  console.log("dayLabels", dayLabels)
-  console.log("dayDatas", dayDatas)
-  console.log("dayDatasPos", dayDatasPos)
-  console.log("dayDatasNeu", dayDatasNeu)
-  console.log("dayDatasNeg", dayDatasNeg)
-
-  const data = {
-    labels: dayLabels,
-    datasets: [
-      {
-        label: 'Positive',
-        data: dayDatasPos
-      },
-      {
-        label: 'Neutral',
-        data: dayDatasNeu
-      },      {
-        label: 'Negative',
-        data: dayDatasNeg
       }
-    ]
+
+      dayDatas.push(dayCount[daily].length)
+      dayDatasPos.push(posCnt)
+      dayDatasNeu.push(neuCnt)
+      dayDatasNeg.push(negCnt)
+
+    }
+    dayLabels.reverse()
+    dayDatas.reverse()
+    
+    dayDatasPos.reverse()
+    dayDatasNeu.reverse()
+    dayDatasNeg.reverse()
+    
+    console.log("dayLabels", dayLabels)
+    console.log("dayDatas", dayDatas)
+    console.log("dayDatasPos", dayDatasPos)
+    console.log("dayDatasNeu", dayDatasNeu)
+    console.log("dayDatasNeg", dayDatasNeg)
+    
+    const data = {
+      labels: dayLabels,
+      datasets: [
+        {
+          label: 'Positive',
+          data: dayDatasPos
+        },
+        {
+          label: 'Neutral',
+          data: dayDatasNeu
+        },      {
+          label: 'Negative',
+          data: dayDatasNeg
+        }
+      ]
+    }
+  }
+  // 시간별 정보 모으기
+  else{
+
   }
 
   return (
     <Wrapper>
+      <DayHourRadio 
+        onChange={(e)=>{
+          setLabelFormat(e.target.value)
+        }}
+        selectedValue={labelFormat}
+      />
       <ChartWrapper>
-        <Line data={data} />
+          <Line 
+            options={{
+              responsive:true,
+              maintainAspectRatio: false
+            }} 
+            data={data} 
+          />
       </ChartWrapper>
     </Wrapper>
   )
